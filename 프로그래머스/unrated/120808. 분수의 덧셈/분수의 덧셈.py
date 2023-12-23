64c7e8ef0ea188565345f6e3992915b0065a1170
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2023-12-24 02:16:31 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,17 +1,9 @@
 def solution(numer1, denom1, numer2, denom2):
-    denom3 = denom1 * denom2 #분모
-    numer3 = (numer1 * denom2) + (numer2 * denom1) #분자
-    
-    i = 2
-    
-    while i <= numer3:
-        if (denom3 % i == 0) & (numer3 % i == 0):
-            denom3 = denom3 // i
-            numer3 = numer3 // i
-            i = 1
-            
-        i = i + 1
-                
-    answer = [numer3,denom3] 
-
+    numer3=(numer1*denom2) + (numer2*denom1)
+    denom3=(denom2*denom1)
+    for i in range (denom3, 1, -1):
+        if numer3%i == 0 and denom3%i == 0:
+            numer3 = numer3//i
+            denom3 = denom3//i
+    answer = [numer3, denom3]
     return answer
