5b514dc1e1bec4a5a76e76fd8267ab16abd17d1e
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2024-01-03 18:06:46 +0900

    [level unrated] Title: 배열 자르기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.py
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.py
@@ -1,5 +1,6 @@
 def solution(numbers, num1, num2):
     answer = []
-    for i in numbers[num1:num2+1]:
-        answer.append(i)
+    for i in range(num1,num2+1):
+        answer.append(numbers[i])
     return answer
+
