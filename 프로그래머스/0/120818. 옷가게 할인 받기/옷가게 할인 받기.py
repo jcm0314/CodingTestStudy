efb2e9e2e2d9ebf0e43f26a9941086fab154704b
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2024-10-02 21:58:43 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,14 @@
+def solution(price):
+
+
+    if price < 100000:
+        answer = price
+    elif 100000 <= price < 300000:
+        answer = price * (95/100)
+    elif 300000 <= price < 500000:
+        answer = price * (90/100)
+    else:
+	    answer = price * (80/100)	
+
+
+    return int(answer)
