5e6aa574d3479b7694d7d9e21509ac9004d45743
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2025-01-27 13:30:35 +0000

    250127

--- 프로그래머스/0/콜라스 수열 만들기.py
+++ 프로그래머스/0/콜라스 수열 만들기.py
@@ -1,12 +1,11 @@
 def solution(n):
     answer = []
-    
-    while n != 1:
+    while (n != 1):
         answer.append(int(n))
-        if n % 2 == 0:
-            n = n / 2
+        if (n % 2 == 0):
+            n = n // 2
         else:
-            n = 3 * n + 1  
+            n = 3 * n + 1
     answer.append(1)
     
     return answer
