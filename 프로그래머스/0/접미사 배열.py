a267e792ff861125ed50549839634916ac2fd7dd
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2025-02-20 09:35:50 +0000

    250220

--- 프로그래머스/0/접미사 배열.py
+++ 프로그래머스/0/접미사 배열.py
@@ -1,6 +1,6 @@
 def solution(my_string):
     answer = []
-    for i in range(len(my_string))
+    for i in range(len(my_string)):
         answer.append(my_string[i:])
             
     return sorted(answer)
