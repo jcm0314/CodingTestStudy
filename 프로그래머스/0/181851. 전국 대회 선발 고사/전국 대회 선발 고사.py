cf7066361a1240af6db853ead189d211714cb601
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2025-03-07 23:09:45 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -0,0 +1,10 @@
+def solution(rank, attendance):
+    selected = []
+    for i, attend in enumerate(attendance):
+        if attend:
+            selected.append((rank[i], i))
+
+    selected.sort()
+    a, b, c = selected[:3]
+
+    return 10000 * a[1] + 100 * b[1] + c[1]
