dd6e776c73af46c3e2fed6dd91aca9db9c391244
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2025-07-31 15:49:46 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 1.22 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        // 0부터 9까지 N 리스트 하나 만들기
+        List<Integer> N = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
+        
+        // N리스트에서 numbers 리스트 빼기
+        for(int i = 0; i < numbers.length; i++) {
+            for(int k = 0; k < N.size(); k++) {
+                if (N.get(k) == numbers[i]) {
+                    N.remove(k);
+                    break; // 중복 제거를 방지하기 위해 break 추가
+                }
+            }
+        }
+        
+        // 남은 N리스트를 다 더하기
+        for(int r = 0; r < N.size(); r++) {
+            sum += N.get(r);
+        }
+        
+        return sum;
+    }
+}
