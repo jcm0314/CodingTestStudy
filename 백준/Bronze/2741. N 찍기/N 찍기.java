b0878e3401e0cb9b5877e29ea2c31eaa7ad8ed25
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-03-24 00:12:48 +0900

    [Bronze V] Title: N 찍기, Time: 728 ms, Memory: 30960 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        
+        for (int i = 1; i < k+1; i++) System.out.println(i);
+    }
+}
