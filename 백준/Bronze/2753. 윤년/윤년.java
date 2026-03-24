d0650d41e6798eac1bb11a000b820ba62f329848
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-03-24 09:30:00 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int year = Integer.parseInt(st.nextToken());
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) System.out.println(1);
+        else System.out.println(0);
+    }
+}
