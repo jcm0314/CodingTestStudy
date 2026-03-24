7202703baecc210e8cb82cb3b957a5510c3da491
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-03-24 20:35:54 +0900

    [Bronze IV] Title: Which Alien?, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+        
+        if (a >= 3 && e <= 4) {
+			System.out.println("TroyMartian");
+		}
+		if (a <= 6 && e >= 2) {
+			System.out.println("VladSaturnian");
+		}
+		if (a <= 2 && e <= 3) {
+			System.out.println("GraemeMercurian");
+		}
+    }
+}
