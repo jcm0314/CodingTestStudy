a30ca94e1133a5034d92dc82e74d22f5e1b7e433
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-03-25 15:19:55 +0900

    [Bronze V] Title: 합, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int e = 0;
+        for (int i = 1; i <= a; i++) {
+            e += i;
+        }
+        
+        System.out.println(e);
+    }
+}
