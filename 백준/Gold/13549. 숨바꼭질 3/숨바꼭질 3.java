cbd687340aecb91caff1bd401c7c7fef593c6dfd
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-03-28 23:29:13 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 128 ms, Memory: 15480 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int MAX = 100000;
+    static final int INF = 1000000000;
+    
+    static int N, K;
+    static int[] dist = new int[MAX + 1];
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        
+        System.out.println(bfs());
+    }
+    
+    static int bfs() {
+        if (N >= K) {
+            return N - K;
+        }
+        
+        Arrays.fill(dist, INF);
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.add(N);
+        dist[N] = 0;
+        
+        while (!dq.isEmpty()) {
+            int x = dq.pollFirst();
+            
+            if (x == K) {
+                return dist[x];
+            }
+            
+            int nx = x * 2;
+            if(nx <= MAX && dist[nx] > dist[x]) {
+                dist[nx] = dist[x];
+                dq.addFirst(nx);
+            }
+            
+            nx = x - 1;
+            if (nx >= 0 && dist[nx] > dist[x] + 1) {
+                dist[nx] = dist[x] + 1;
+                dq.addLast(nx);
+            }
+            nx = x + 1;
+            if (nx <= MAX && dist[nx] > dist[x] + 1) {
+                dist[nx] = dist[x] + 1;
+                dq.addLast(nx);
+            }
+              
+             
+        }
+        return dist[K];
+    }
+    
+}
