3a83f1e59cb79f8bbcbaf03830fb5b61fc9b6fb3
jcm0314 <143155281+jcm0314@users.noreply.github.com>
2026-05-14 23:27:01 +0900

    [level 2] Title: 올바른 괄호, Time: 3.02 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,16 +1,22 @@
-import java.util.Stack;
-
 class Solution {
-        boolean solution(String s) {
+    boolean solution(String s) {
+        boolean answer = false;
         int count = 0;
-        int len = s.length();
-        for (int i = 0; i < len; i++) {
-            count += s.charAt(i) == '(' ? 1 : -1;
-
-            if (count < 0) {
-                return false;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
             }
         }
-        return count == 0;
+        if(count == 0){
+            answer = true;
+        }
+
+        return answer;
     }
 }
